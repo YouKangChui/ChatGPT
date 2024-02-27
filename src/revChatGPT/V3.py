9d912e58dc3ea2ed09f03df6ff2a452307c64ebd
YouKangChui <104844105+YouKangChui@users.noreply.github.com>
2024-02-27 23:12:04 +0800

    Update V3.py

--- src/revChatGPT/V3.py
+++ src/revChatGPT/V3.py
@@ -34,6 +34,11 @@ ENGINES = [
     "gpt-4-32k-0314",
     "gpt-4-0613",
     "gpt-4-32k-0613",
+    "gpt-3.5-turbo-0125",
+    "gpt-3.5-turbo-1106",
+    "gpt-4-0125-preview",
+    "gpt-4-1106-preview",
+    "gpt-4-turbo-preview",
 ]
 
 
