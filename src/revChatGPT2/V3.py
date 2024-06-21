7f135340eebbb4154658b364aaf226685b9a5ca5
YouKangChui <104844105+YouKangChui@users.noreply.github.com>
2024-06-21 09:13:16 +0800

    Update V3.py

--- src/revChatGPT2/V3.py
+++ src/revChatGPT2/V3.py
@@ -49,7 +49,9 @@ ENGINES = [
     "claude-3-sonnet-20240229",
     "claude-3-haiku-20240307",
     "claude_3_opus",
-    "claude-3-opus"
+    "claude-3-opus",
+    "claude-3-5-sonnet-20240620"
+    "claude-3.5-sonnet-20240620"
 ]
 
 
